e76a3438039cdc5aa485d004102d8414b84236d3
Brian Peterson <bpeterson1579@gmail.com>
2021-07-11 20:26:32 -0600

    Finished 3/4 of it.

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,27 @@
+function shelfBook(bookTitle, sciFiShelf) {
+  if (bookTitle.genre === "sciFi" && sciFiShelf.length < 3) {
+    sciFiShelf.unshift(bookTitle);
+  }else {
+    return
+  }
+}
+
+function unshelfBook(bookTitle, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    var takeBook = shelf[i].title.includes(bookTitle);
+    if (takeBook === true) {
+      return shelf.splice(i, 1);
+    }
+  }
+}
+
+function listTitles(fantasyShelf) {
+  return fantasyShelf.title;
+}
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
+  shelfBook,
+  unshelfBook,
+  listTitles,
   // searchShelf
 };
