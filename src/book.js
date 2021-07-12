4a62a904eb6ef490c52d16153898f91b5bf2c742
Brian Peterson <bpeterson1579@gmail.com>
2021-07-11 20:25:20 -0600

    Finished book.js

--- src/book.js
+++ src/book.js
@@ -1,12 +1,47 @@
-function createTitle() {
-  
+function createTitle(bookIdea) {
+  return `The ${bookIdea}`;
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  var newCharacter = {
+    name: name,
+    age: age,
+    pronouns: pronouns,
+  }
+  return newCharacter;
+}
+
+function saveReview(reviewText, reviews) {
+  for (var i = 0; i < reviews.length; i++) {
+    if (reviews[i] === reviewText) {
+      return
+    }
+  }
+  reviews.push(reviewText);
+}
+
+function calculatePageCount(createTitle) {
+  return createTitle.length * 20;
+}
+
+function writeBook(bookTitle, bookCharacter, genre) {
+  return book = {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: calculatePageCount(bookTitle),
+    genre: genre,
+  }
+}
+
+function editBook(ghoulBook) {
+  ghoulBook.pageCount = ghoulBook.pageCount * .75;
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
